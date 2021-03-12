7e28ba8b10de52963e8248842f1d9836727ea38e
Alexander Aberer <alexander.aberer@omicronenergy.com>
2021-03-12 19:17:50 +0100

    cleanup mess

---
+++ src/Samples/NetCoreWithDIServer/InstanceProvider.cs
@@ -0,0 +1,22 @@
+﻿using CoreWCF;
+using CoreWCF.Channels;
+using CoreWCF.Dispatcher;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace NetCoreWithDIServer
+{
+    public class InstanceProvider<TContract> : IInstanceProvider
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public InstanceProvider(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public object GetInstance(InstanceContext instanceContext, Message message) => _serviceProvider.GetService<TContract>();
+        public object GetInstance(InstanceContext instanceContext) => _serviceProvider.GetService<TContract>();
+        public void ReleaseInstance(InstanceContext instanceContext, object instance) { }
+    }
+}
---
+++ src/Samples/NetCoreWithDIServer/MessageQueue.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+
+namespace NetCoreWithDIServer
+{
+    public class MessageQueue : IMessageQueue
+    {
+        private readonly ConcurrentQueue<string> _queue = new ConcurrentQueue<string>();
+        public string Dequeue()
+        {
+            if (_queue.TryDequeue(out string message)) return message;
+            return string.Empty;
+        }
+
+        public void Enqueue(string message) => _queue.Enqueue(message);
+    }
+}

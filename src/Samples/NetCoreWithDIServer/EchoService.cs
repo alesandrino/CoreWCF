7d9bf04653cf3b0f87289a8d5bb4c8e93e6413db
Alexander Aberer <alexander.aberer@omicronenergy.com>
2021-03-12 19:04:39 +0100

    implement DI sample

---
+++ src/Samples/NetCoreWithDIServer/EchoService.cs
@@ -0,0 +1,33 @@
+﻿using Contract;
+
+namespace NetCoreWithDIServer
+{
+
+    public class EchoService : Contract.IEchoService
+    {
+        private readonly IMessageQueue _messageQueue;
+
+        public EchoService(IMessageQueue messageQueue)
+        {
+            _messageQueue = messageQueue;
+        }
+
+        public string Echo(string text)
+        {
+            System.Console.WriteLine($"Received {text} from client!");
+            string message = _messageQueue.Dequeue();
+            System.Console.WriteLine($"The message before was {message}!");
+            _messageQueue.Enqueue(text);
+            return text;
+        }
+
+        public string ComplexEcho(EchoMessage text)
+        {
+            System.Console.WriteLine($"Received {text.Text} from client!");
+            string message = _messageQueue.Dequeue();
+            System.Console.WriteLine($"The message before was {message}!");
+            _messageQueue.Enqueue(text.Text);
+            return text.Text;
+        }
+    }
+}
---
+++ src/Samples/NetCoreWithDIServer/IEchoService.cs
@@ -0,0 +1,22 @@
+﻿using CoreWCF;
+using System.Runtime.Serialization;
+
+namespace Contract
+{
+    [ServiceContract]
+    public interface IEchoService
+    {
+        [OperationContract]
+        string Echo(string text);
+
+        [OperationContract]
+        string ComplexEcho(EchoMessage text);
+    }
+
+    [DataContract]
+    public class EchoMessage
+    {
+        [DataMember]
+        public string Text { get; set; }
+    }
+}
